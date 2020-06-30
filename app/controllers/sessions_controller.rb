db0ef58cc100c86fddf25c9963c2c84908e0e3d6
iona-b <ionabrabender@gmail.com>
2020-06-30 16:18:40 -0700

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if params[:name] == nil
+      redirect_to controller: 'sessions', action: 'new'
+    elsif params[:name] == ""
+      redirect_to controller: 'sessions', action: 'new'
+    else
+      params[:name] != nil
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to '/login'
+  end
+
+end
